2300de6dd3543d4b58be053f48a852f1d56ba1e8
quochuy278 <quochuy270800@gmail.com>
2023-05-25 16:42:24 +0300

    add icons

---
+++ src/components/icons/index.ts
@@ -0,0 +1,9 @@
+import { GithubIcon } from "./Github";
+import { LinkedInIcon } from "./Linkedin";
+import { Logo } from "./Logo";
+
+export interface IconsProps {
+  className?: string;
+}
+
+export { GithubIcon, LinkedInIcon, Logo };
