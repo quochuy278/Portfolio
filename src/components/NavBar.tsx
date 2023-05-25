31cbfc5e82b9596ccfb6edd8d6b6a6dccca0ba12
quochuy278 <quochuy270800@gmail.com>
2023-05-25 16:43:14 +0300

    add nav bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { CustomLink } from "./custom";
+import { GithubIcon, LinkedInIcon, Logo } from "./icons";
+
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <header
+      className="w-full px-32 py-8 font-medium flex items-center justify-between
+    dark:text-light relative z-10 lg:px-16 md:px-12 sm:px-8
+    "
+    >
+      <button
+        className=" flex-col justify-center items-center hidden lg:flex"
+        onClick={handleClick}
+      >
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm  ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          } `}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          } `}
+        ></span>
+      </button>
+      <div className="w-full flex justify-between items-center ">
+        <nav className="flex items-center justify-center">
+          <CustomLink href="/" title="Home" className="mr-4" />
+          <CustomLink href="/about" title="About" className="mx-4" />
+          <CustomLink href="/projects" title="Projects" className="mx-4" />
+          <CustomLink href="/articles" title="Articles" className="ml-4" />
+        </nav>
+
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://twitter.com"
+            target={"_blank"}
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <GithubIcon />
+          </motion.a>
+          <motion.a
+            href="https://twitter.com"
+            target={"_blank"}
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <LinkedInIcon />
+          </motion.a>
+
+          {/* <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-3 flex items-center justify-center rounded-full p-1 ease
+      ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
+      `}
+          >
+            {mode === "light" ? (
+              <SunIcon className={"fill-dark"} />
+            ) : (
+              <MoonIcon className={"fill-dark"} />
+            )}
+          </button> */}
+        </nav>
+      </div>
+      <div className="absolute left-[50%] top-2 translate-x-[-50%]">
+        <Logo />
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
