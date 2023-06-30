32a2881b8d5d9f3b4df0cd10c9e91c45bd95e79d
Huy Bui <quochuy270800@gmail.com>
2023-06-30 15:57:55 +0300

    add sanity client service to fetch real time data

--- src/sanity-client/sanityClient.ts
+++ src/sanity-client/sanityClient.ts
@@ -1,5 +1,5 @@
 // sanity.js
-import {createClient} from '@sanity/client'
+import { createClient } from "@sanity/client";
 // Import using ESM URL imports in environments that supports it:
 // import {createClient} from 'https://esm.sh/@sanity/client'
 
@@ -7,22 +7,27 @@ export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_DATASET_NAME,
   useCdn: true, // set to `false` to bypass the edge cache
-  apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
+  apiVersion: "2023-05-03", // use current date (YYYY-MM-DD) to target the latest API version
   // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
-})
+});
 
 // // uses GROQ to query content: https://www.sanity.io/docs/groq
 export async function getSkills() {
-  const skills = await client.fetch(`*[_type == "Skills"]`)
-  return skills
+  const skills = await client.fetch(`*[_type == "Skills"]`);
+  return skills;
 }
 
-// export async function createPost(post: Post) {
-//   const result = client.create(post)
-//   return result
-// }
+export async function getExperiences() {
+  const experience = await client.fetch(`*[_type == "Experience"]`);
+  return experience;
+}
 
-// export async function updateDocumentTitle(_id, title) {
-//   const result = client.patch(_id).set({title})
-//   return result
-// }
+export async function getEducations() {
+  const educations = await client.fetch(`*[_type == "Education"]`);
+  return educations;
+}
+
+export async function getBiography() {
+  const biography = await client.fetch(`*[_type == "Biography"]`);
+  return biography;
+}
