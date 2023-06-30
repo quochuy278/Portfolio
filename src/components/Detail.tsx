9b4fdb8028c006299186ab60ec61454ded0d07b9
Huy Bui <quochuy270800@gmail.com>
2023-06-30 16:03:44 +0300

    spliting detail and lilcon component from skill component

---
+++ src/components/Detail.tsx
@@ -0,0 +1,29 @@
+import { useRef } from "react";
+import LiIcon from "./Lilcon";
+import { motion, useScroll } from "framer-motion";
+const Details = ({ type, time, place, info }: any) => {
+  const ref = useRef(null);
+  return (
+    <li
+      ref={ref}
+      className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-center justify-between "
+    >
+      <LiIcon reference={ref} />
+      <motion.div
+        initial={{ y: 50 }}
+        whileInView={{ y: 0 }}
+        transition={{ duration: 0.5, type: "spring" }}
+      >
+        <h3 className="capitalize font-bold text-2xl sm:text-xl xs:text-lg">
+          {type}
+        </h3>
+        <span className="capitalize font-medium text-dark/75 dark:text-light/75 ">
+          {time} | {place}
+        </span>
+        <p className="font-medium w-full md:text-sm">{info}</p>
+      </motion.div>
+    </li>
+  );
+};
+
+export {Details}
