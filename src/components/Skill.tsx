392d8aa2cba7d45bf4552c6bdb06e048ec50b5e1
Huy Bui <quochuy270800@gmail.com>
2023-06-30 15:59:14 +0300

    add skill component

---
+++ src/components/Skill.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import LoadingSpinner from "./icons/LoadingSpinner";
+import Image from "next/image";
+import imageUrlBuilder from "@sanity/image-url";
+import { client } from "@/sanity-client/sanityClient";
+
+const builder = imageUrlBuilder(client);
+const urlFor = (source: string) => {
+  return builder.image(source);
+};
+
+const Skill = ({ skill }: any) => {
+  if (!skill) {
+    return <LoadingSpinner />;
+  }
+
+  const imageUrl = urlFor(skill.Icon.icon[0].asset._ref).url();
+
+  return (
+    <li className="w-full h-18 flex items-center justify-center p-3">
+      <div className="flex flex-col justify-center items-center">
+        <Image
+          src={imageUrl}
+          alt={skill.Icon.title}
+          width={skill.Icon.Width}
+          height={skill.Icon.Height}
+        />
+        <div className="font-bold my-1">{skill.name}</div>
+      </div>
+    </li>
+  );
+};
+
+export default Skill;
---
+++ src/components/Skills.tsx
@@ -0,0 +1,21 @@
+import Skill from "./Skill";
+
+const Skills = ({ skills }: any) => {
+  return (
+    <div className="my-24">
+      <h2 className="font-bold text-8xl mb-32 w-full text-center md:text-6xl xs:text-4xl md:mb-16">
+        Skills
+      </h2>
+
+      <div className="w-full flex justify-center items-center">
+        <ul className="w-full grid grid-cols-4 gap-2 gap-y-5 2xl:grid-cols-6 xl:grid-cols-5 ">
+          {skills.map((skill: any) => {
+            return <Skill skill={skill} key={skill._id} />;
+          })}
+        </ul>
+      </div> 
+    </div>
+  );
+};
+
+export default Skills;
